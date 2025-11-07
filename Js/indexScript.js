0e5af49b6b49b2f52ea8ec87595688a1d44cb5fd
Dr-Discount <trackerjack02@gmail.com>
2025-11-07 02:29:31 +0000

    finished

--- Js/indexScript.js
+++ Js/indexScript.js
@@ -1,1 +1,18 @@
-var x = 10;
+var slides = [];
+slides[0] = "Images/doom.png";
+slides[1] = "Images/minecraft.png";
+slides[2] = "Images/repo.png";
+slides[3] = "Images/silksong.png";
+
+var step = 0;
+let slider = () => {
+    document.slideshow.src = slides[step];
+    if (step < slides.length - 1) {
+        step++;
+    } else {
+        step = 0;
+    }
+    setTimeout("slider()", 5000);
+}
+
+window.onload = slider;
